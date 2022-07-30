b1fa34e34dfed408387a7eee317c3dfd8e0dadf2
Toan Dao <toanddao@gmail.com>
2022-07-29 23:06:20 -0700

    Fix query for getReviews to include array of photos

--- server/models.js
+++ server/models.js
@@ -14,8 +14,8 @@ const getReviews = (product_id, count, page, sort) => {
     sort = `reviews.date DESC, reviews.helpfulness DESC`;
   }
   let queryString = `SELECT reviews.id, reviews.rating, reviews.date, reviews.summary, reviews.body, reviews.recommend,
-  reviews.reported, reviews.reviewer_name, reviews.reviewer_email, reviews.response, reviews.helpfulness, photos.url
-  FROM reviews JOIN photos ON reviews.id = photos.review_id WHERE reviews.product_id = ${product_id} AND reviews.reported = false
+  reviews.reviewer_name, reviews.reviewer_email, reviews.response, reviews.helpfulness, json_agg( COALESCE (json_build_object('id', photos.id, 'url', photos.url), '[]')) AS photos
+  FROM reviews LEFT JOIN photos ON reviews.id = photos.review_id WHERE reviews.product_id = ${product_id} AND reviews.reported = false GROUP BY reviews.id
   order by ${sort} offset ${offset} limit ${count}`;
   // pool.query(queryString, (err) => {
   //   console.log('query string: ', queryString);
@@ -27,3 +27,5 @@ const getReviews = (product_id, count, page, sort) => {
 }
 
 module.exports.getReviews = getReviews;
+
+// (SELECT json_agg(url) FROM photos WHERE photos.review_id = reviews.id) AS url
